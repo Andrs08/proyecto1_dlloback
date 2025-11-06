db3374773ade48e31014708f9fd6c6cbe0e547c4
andrs08 <chinchillahuelvas@gmail.com>
2025-11-05 23:54:36 -0500

    Adicion de los endpoint de los puntos 2, 3 y 4 del taller 1

--- src/taller1/taller1.routes.ts
+++ src/taller1/taller1.routes.ts
@@ -4,15 +4,47 @@ import {convertidorTemp, resolvedor, mejorParidad, peorParidad} from "./taller1.
 const taller1Routes = Router();
 
 async function convertirTemp(request: Request, response: Response) {
-    const gradosCentigrados = request.body;
+    const {gradosCentigrados} = request.body;
     const resultado = await convertidorTemp(gradosCentigrados)
     
     response.status(200).json({
-        temperatura_fahrenheit: resultado
+        "temperatura_fahrenheit": resultado
     })
 }
 
 taller1Routes.post("/punto1", convertirTemp)
-console.log("taller1.routes.ts cargado correctamente");
+
+async function ecuacion(request: Request, response: Response){
+    const {valorA, valorB, valorC, raizPositiva} = request.body;
+    const resultado = await resolvedor(valorA, valorB, valorC, raizPositiva);
+
+    response.status(200).json ({
+        "x": resultado 
+    })
+}
+
+taller1Routes.post("/punto2", ecuacion);
+
+async function mejorPar(request: Request, response: Response) {
+    const {numero} = request.body;
+    const resultado = await mejorParidad(numero)
+
+    response.status(200).json ({
+        "es_par": resultado
+    })
+}
+
+taller1Routes.post("/punto3", mejorPar);
+
+async function peorPar (request: Request, response: Response) {
+    const {numero} = request.body;
+    const resultado = await peorParidad(numero);
+
+    response.status(200).json ({
+        "es_par": resultado
+    })
+}
+
+taller1Routes.post("/punto4", peorPar);
 
 export default taller1Routes;
